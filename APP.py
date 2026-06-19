377bee59d1a6146cd64075629b31feca87dbac88
abhinavramesh943-art <abhinavramesh943@gmail.com>
2026-06-19 23:30:00 +0530

    Update APP.py

--- APP.py
+++ APP.py
@@ -19,14 +19,6 @@ df = df.dropna()
 df = df.drop_duplicates()
 
 
-import matplotlib.pyplot as plt
-
-plt.boxplot(df['Sales Count'])
-plt.show()
-
-
-
-
 
 
 df['Hour'] = df['Timestamp'].dt.hour
@@ -51,34 +43,6 @@ df['NetMovement'] = (df['Sales Count'] - df['Redemption Count'])
 
 
 
-
-hourly_sales = df.groupby('Hour')['Sales Count' ].mean()
-
-
-hourly_redemption = df.groupby('Hour')['Redemption Count' ].mean()
-
-
-
-
-import matplotlib.pyplot as plt
-hourly_sales.plot(figsize=(10,5))              
-plt.title("Average Ticket Sales by Hour")   
-plt.show()
-
-
-df.groupby('Weekend')['Sales Count'].mean()
-
-
-monthly = df.groupby('Month')['Sales Count'].mean()
-monthly.plot()
-plt.show()
-
-
-df['Rolling1Hour'] = df['Sales Count'].rolling(4).mean()
-                                                                                                          
-df['Rolling4Hour'] = df['Sales Count'].rolling(16).mean()
-
-peak = df.nlargest(20, 'Sales Count') 
                                         
 import streamlit as st                                                                                                                                                    
                                                         
